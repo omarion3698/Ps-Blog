416567539578e2729fe4d89e5597e9174f62e2d7
omarion3698 <omaribinbakarivic@gmail.com>
2020-09-29 21:08:40 +0300

    Edit manage file

--- manage.py
+++ manage.py
@@ -4,8 +4,7 @@ from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 
 # creating an app instance
-app = create_app("development")
-# app = create_app('production')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
